e0c0ba84fc05eeaa70a27fd9b9c13eecb8f7cec3
Sai-Ganesh-Gowd-Vemana <saiganeshvemana2403@gmail.com>
2026-04-20 16:36:24 +0530

    feat: use env variable for API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
 export const detectSMS = async (message) => {
   const response = await fetch(`${API_BASE}/detect-sms`, {
